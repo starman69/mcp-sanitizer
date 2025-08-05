655df7c33872f602a02d47b3c55e1321de37bf24
starman69 <dave@pattens.org>
2025-08-05 11:30:59 -0700

    add config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-new-func': 'error',
+    'no-script-url': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  overrides: [
+    {
+      files: ['test/**/*.js', '**/*.test.js'],
+      rules: {
+        'no-console': 'off'
+      }
+    }
+  ]
+};
---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!src/types/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85
+    }
+  },
+  testMatch: [
+    '**/test/**/*.test.js',
+    '**/__tests__/**/*.js'
+  ],
+  testTimeout: 10000,
+  verbose: true
+};
